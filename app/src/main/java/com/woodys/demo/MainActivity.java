e8a00d8ce667560a5007c4f45705fb0b8d1f6bcc
tao.yue <12345678>
2017-09-13 10:44:46 +0800

    完善相关功能

--- app/src/main/java/com/woodys/demo/MainActivity.java
+++ app/src/main/java/com/woodys/demo/MainActivity.java
@@ -3,7 +3,7 @@ package com.woodys.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.woodys.eventcollect.R;
+import com.woodys.demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
